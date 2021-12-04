d83c52b0d31d5b27f5584142ef367b016157667c
saymon shoab <mdsaymonshoab@gmail.com>
2021-12-04 18:42:05 +0600

    style items component

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Items from './Components/Items';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.teskwrapper}>
+        <Text style={styles.projecttitle}>Sub-Goyal-Job-Task</Text>
+      <View style={styles.items}>
+          <Items text={'item-1'} />
+          <Items text={'item-2'} />
+          <Items text={'item-3'} />
+       </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  teskwrapper:{
+    paddingTop:60,
+    paddingHorizontal:20
+  },
+  projecttitle:{
+    fontSize:20,
+    fontWeight:'bold',
+},
+  items:{
+     marginTop:30
+  }
+
 });
