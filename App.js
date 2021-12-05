c78ae3cf7759d40da022b7a3c13db37d1fc3ad32
saymon shoab <mdsaymonshoab@gmail.com>
2021-12-06 02:43:42 +0600

    add items component

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Items from './Components/Items';
 
 export default function App() {
+  const [item, setItem] = useState();
+  const [items, setItems] = useState([]);
+
+  const handleAddItem = () => {
+    console.log(item);
+    setItems([...items, item]);
+    setItem(null);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.teskwrapper}>
-        <Text style={styles.projecttitle}>Sub-Goyal-Job-Task</Text>
+        <Text style={styles.projecttitle}>SUB-GOYAL-ASSIGNMENT</Text>
+       <KeyboardAvoidingView
+          behavior={Platform.OS == 'ios'? "padding": "height"}
+          style={styles.writeItemWrapper}
+       >
+          <TextInput style={styles.input} placeholder="write a item" value={item} onChangeText={text => setItem(text) }/>
+          <TouchableOpacity onPress={()=> handleAddItem()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+       </KeyboardAvoidingView> 
       <View style={styles.items}>
-          <Items text={'item-1'} />
-          <Items text={'item-2'} />
-          <Items text={'item-3'} />
+        {
+          items.map((item)=> {
+           return <Items
+            text={item} />
+          })
+        }
+          
+      
        </View>
       </View>
     </View>
@@ -30,9 +55,41 @@ const styles = StyleSheet.create({
   projecttitle:{
     fontSize:20,
     fontWeight:'bold',
+    marginTop:20,
+    marginBottom:20,
+    textAlign:'center'
 },
   items:{
      marginTop:30
+  },
+  writeItemWrapper:{
+    // position:'absolute',
+    width:'100%',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  input:{
+    paddingVertical:15,
+    paddingHorizontal:15,
+    backgroundColor:"#fff",
+    borderRadius:10,
+    borderColor:"#C0C0C0",
+    borderWidth:1,
+    width:290,
+  },
+  addWrapper:{
+    width:60,
+    height:60,
+    backgroundColor:'#fff',
+    justifyContent:'center',
+    alignItems:'center',
+    borderColor:'#C0C0C0',
+    borderWidth:1,
+    borderRadius:10
+  },
+  addText:{
+    fontSize:30,
+    
   }
-
 });
